a3a6379ea8ec88fc102a953ab218e24b0aa2aef3
s1250053 <s1250053@std1dc36.u-aizu.ac.jp>
2018-07-06 00:45:18 +0900

    Won/lost message modified

--- Dice.c
+++ Dice.c
@@ -2,7 +2,12 @@
 
 int main(){
   int sum=0,i,num;
+  char name[100];
 
+  printf("What is your name?\n> ");
+  scanf("%s",name);
+  printf("Hello, %s!\n",name);
+  
   srand(time(NULL)); // 現在時刻の情報で初期化
   printf("Rolling the dice...\n");
   for(i = 0; i < 2; i++) {
@@ -13,6 +18,9 @@ int main(){
   }
 
   printf("Total value: %d\n",sum);
+
+  if(sum > 7) printf("%s won!\n",name);
+  else printf("%s lost!\n",name);
   
   return 0;
 }
